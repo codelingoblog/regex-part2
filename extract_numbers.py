e04781b04d7fcb7d8dec7a4a41a4084b294535ec
codelingoblog <codelingoblog@gmail.com>
2017-02-22 23:32:26 +0530

    Update extract_numbers.py

--- extract_numbers.py
+++ extract_numbers.py
@@ -1,11 +1,9 @@
 import re
 
 # regex to match Washington D.C phone numbers
-
 regex = r"\b202\-\d{3}\-\d{4}"
 
 file = open("phone_numbers.txt", "r")
 phonenumbers = file.read()
 
 print(re.findall(regex, phonenumbers))
-
